e56c3e21b148de8b66100841d5a6c9c8735168b9
locslr <sreelakhmir1996@gmail.com>
2018-03-24 13:04:43 +0530

    server modified

--- UDPServer.java
+++ UDPServer.java
@@ -3,12 +3,11 @@ import java.net.*;
  
 class UDPServer
 {
-
-public static void main(String args[]) throws Exception
+   public static void main(String args[]) throws Exception
       {
          DatagramSocket serverSocket = new DatagramSocket(9876);
             byte[] receiveData = new byte[1024];
-             byte[] sendData = new byte[1024];
+            byte[] sendData = new byte[1024];
             while(true)
                {
                   DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
@@ -17,12 +16,11 @@ public static void main(String args[]) throws Exception
                   System.out.println("RECEIVED: " + sentence);
                   InetAddress IPAddress = receivePacket.getAddress();
                   int port = receivePacket.getPort();
-                    String capitalizedSentence = sentence.toUpperCase();
+                  String capitalizedSentence = sentence.toUpperCase();
                   sendData = capitalizedSentence.getBytes();
                   DatagramPacket sendPacket =
                   new DatagramPacket(sendData, sendData.length, IPAddress, port);
                   serverSocket.send(sendPacket);
                }
       }
-
 }
